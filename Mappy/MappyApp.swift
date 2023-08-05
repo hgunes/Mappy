126a6853d6725c7e5a3ca0d74090606e02a4e628
harungunes <h.gunes13@gmail.com>
2023-08-05 13:43:14 +0200

    init repo

---
+++ Mappy/MappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappyApp.swift
+//  Mappy
+//
+//  Created by Harun Gunes on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mappy/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  Mappy
+//
+//  Created by Harun Gunes on 05/08/2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
