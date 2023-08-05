dd945fd6fb1cff248f416cf260cbdda80a21fbe7
harungunes <h.gunes13@gmail.com>
2023-08-05 15:56:52 +0200

    feat: add map view

--- Mappy/ViewModel/LocationViewModel.swift
+++ Mappy/ViewModel/LocationViewModel.swift
@@ -6,11 +6,34 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationViewModel: ObservableObject {
+    
     @Published var locations: [Location]
     
+    @Published var mapLocation: Location {
+        didSet {
+            updateRegion(mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
-        self.locations = LocationServices.locations
+        let locations = LocationServices.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        updateRegion(locations.first!)
+    }
+    
+    private func updateRegion(_ location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 }
--- Mappy/Views/LocationView.swift
+++ Mappy/Views/LocationView.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
     
     @EnvironmentObject private var vm: LocationViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                VStack {
+                    Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                        .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                }
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 20, y: 15)
+                .padding()
+                
+                Spacer()
             }
         }
     }
